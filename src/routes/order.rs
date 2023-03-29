f46e32b374024a6812eb278b2aa35c9be6e69125
Wredi <piotr.skora69@gmail.com>
2023-03-29 19:32:21 +0200

    order adding works

--- src/routes/order.rs
+++ src/routes/order.rs
@@ -1,7 +1,7 @@
 use actix_web::{post, web};
 use chrono::{NaiveDate, FixedOffset, DateTime, TimeZone};
-use entity::{dinner_orders, dinner};
-use log::error;
+use entity::{dinner_orders, dinner, user_dinner_orders, extras_order};
+use log::{error, info};
 use sea_orm::{Set, EntityTrait};
 
 use crate::{jwt_auth::AuthUser, errors::ServiceError, routes::structs::OrderRequest, appstate::AppState};
@@ -21,10 +21,38 @@ async fn create_order(user: AuthUser, data: web::Data<AppState>, order: web::Jso
         ..Default::default()
     };
 
-    let result = dinner_orders::Entity::insert(dinner_order).exec(db).await.map_err(|e| {
+    let dinner_order_res = dinner_orders::Entity::insert(dinner_order).exec(db).await.map_err(|e| {
         error!("Database error creating order: {}", e);
         ServiceError::InternalError
     })?;
 
-    Ok(format!("Order created with id: {}", result.last_insert_id))
+    let dinner_order_junction = user_dinner_orders::ActiveModel {
+        order_id: Set(dinner_order_res.last_insert_id),
+        dinner_id: Set(order.dinner_id),
+        ..Default::default()
+    };
+
+    let dinner_order_res = user_dinner_orders::Entity::insert(dinner_order_junction).exec(db).await.map_err(|e| {
+        error!("Database error creating order: {}", e);
+        ServiceError::InternalError
+    })?;
+
+    info!("Order created with id: {}", dinner_order_res.last_insert_id);
+
+    let vector = order.extras_ids.into_iter().map(|extra_id|{
+        extras_order::ActiveModel {
+            user_dinner_id: Set(dinner_order_res.last_insert_id),
+            extras_id: Set(extra_id),
+            ..Default::default()
+        }
+    }).collect::<Vec<_>>();
+
+    info!("Extras: {:?}", vector);
+
+    let extras_order_res = extras_order::Entity::insert_many(vector).exec(db).await.map_err(|e| {
+        error!("Database error creating order: {}", e);
+        ServiceError::InternalError
+    })?;
+
+    Ok(format!("Order created with id: {}", extras_order_res.last_insert_id))
 }
